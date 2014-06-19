f149b4be620a65934e6225d49adf07b7857a74ab
ubuntu <michael@blumetech.com>
2014-06-19 11:01:42 -0400

    Initial commit

---
+++ src/Practical_Programming_in_C_6.087.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : 087.c
+ Author      : Michael Raposa
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
